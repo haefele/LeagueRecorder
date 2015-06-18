db82100a46bde8691ad6f1ca1f23dffb0bc84036
Daniel Häfele <haefele@xemio.net>
2015-06-18 21:37:33 +0200

    Added logging to azure table storage.
    Fixes #7.

--- src/Azure/LeagueRecorder.Azure.WorkerRole/WorkerRole.cs
+++ src/Azure/LeagueRecorder.Azure.WorkerRole/WorkerRole.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Anotar.NLog;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
 using LeagueRecorder.Worker.Api;
@@ -27,37 +28,61 @@ namespace LeagueRecorder.Azure.WorkerRole
 
         public override bool OnStart()
         {
-            // Set the maximum number of concurrent connections
-            ServicePointManager.DefaultConnectionLimit = 256;
+            try
+            {
+                // Set the maximum number of concurrent connections
+                ServicePointManager.DefaultConnectionLimit = 256;
 
-            var container = new WindsorContainer();
-            container.Install(FromAssembly.This());
+                var container = new WindsorContainer();
+                container.Install(FromAssembly.This());
 
-            this._cancellationTokenSource = new CancellationTokenSource();
+                this._cancellationTokenSource = new CancellationTokenSource();
 
-            this._summonerInGameFinderWorker = container.Resolve<SummonerInGameFinderWorker>();
-            this._recorderWorker = container.Resolve<RecorderWorker>();
-            this._apiWorker = container.Resolve<ApiWorker>();
+                this._summonerInGameFinderWorker = container.Resolve<SummonerInGameFinderWorker>();
+                this._recorderWorker = container.Resolve<RecorderWorker>();
+                this._apiWorker = container.Resolve<ApiWorker>();
 
-            Task.WaitAll(
-                this._summonerInGameFinderWorker.StartAsync(),
-                this._recorderWorker.StartAsync(),
-                this._apiWorker.StartAsync());
+                Task.WaitAll(
+                    this._summonerInGameFinderWorker.StartAsync(),
+                    this._recorderWorker.StartAsync(),
+                    this._apiWorker.StartAsync());
 
-            return true;
+                return true;
+            }
+            catch (Exception exception)
+            {
+                LogTo.ErrorException("Exception while starting the cloud service.", exception);
+                throw;
+            }
         }
 
         public override void OnStop()
         {
-            this._cancellationTokenSource.Cancel();
+            try
+            {
+                this._cancellationTokenSource.Cancel();
+            }
+            catch (Exception exception)
+            {
+                LogTo.ErrorException("Exception while stopping the cloud service.", exception);
+                throw;
+            }
         }
 
         public override void Run()
         {
-            Task.WaitAll(
-                this._summonerInGameFinderWorker.RunAsync(this._cancellationTokenSource.Token),
-                this._recorderWorker.RunAsync(this._cancellationTokenSource.Token),
-                this._apiWorker.RunAsync(this._cancellationTokenSource.Token));
+            try
+            {
+                Task.WaitAll(
+                    this._summonerInGameFinderWorker.RunAsync(this._cancellationTokenSource.Token),
+                    this._recorderWorker.RunAsync(this._cancellationTokenSource.Token),
+                    this._apiWorker.RunAsync(this._cancellationTokenSource.Token));
+            }
+            catch (Exception exception)
+            {
+                LogTo.ErrorException("Exception while running the cloud service.", exception);
+                throw;
+            }
         }
     }
 }
